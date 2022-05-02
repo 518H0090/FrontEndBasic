189c378e587b6d883134b6fe83a336db74d177b9
518H0090 <nhoxhieuro5@gmail.com>
2022-05-02 16:51:13 +0700

    comment variable JS

--- JS/js_part1/app.js
+++ JS/js_part1/app.js
@@ -1,1 +1,24 @@
-alert("Show Alert");
+var fullName = "Huỳnh Trần Trung Hiếu";
+
+alert(fullName);
+// Đây là comment
+
+/* 
+Đây là comment dài
+Đây là comment dài
+Đây là comment dài
+Đây là comment dài
+Đây là comment dài
+*/
+
+/**
+ * Đây là comment dài
+ * Đây là comment dài
+ * Đây là comment dài
+ * Đây là comment dài
+ * Đây là comment dài
+ * Đây là comment dài
+ */
+
+// Vô hiệu hóa code
+// alert(fullName);
