83c4ef1edd92635e4bed55e5e7b98bf2776379cd
518H0090 <nhoxhieuro5@gmail.com>
2022-05-03 12:39:16 +0700

    string concentration template string

---
+++ JS/js_part4/app.js
@@ -0,0 +1,14 @@
+var firstName = "Trung";
+var lastName = "Hieu";
+
+console.log(firstName + lastName);
+
+console.log(`${firstName} ${lastName} `);
+
+console.log(firstName + " " + lastName);
+
+firstName += lastName;
+
+var fullName = firstName;
+
+console.log(fullName);
