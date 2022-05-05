d76c6466b0b0d071b4cf31f60b4217f110a0ffd6
518H0090 <nhoxhieuro5@gmail.com>
2022-05-05 20:00:45 +0700

    comment

---
+++ JS/js_part5/app.js
@@ -0,0 +1,22 @@
+// if else
+/**
+ * null
+ * 0
+ * Nan
+ * false
+ * '' or ""
+ * undefined
+ * khi duoc convert sang boolean thi la false
+ * còn lại thì là true
+ */
+
+var isSucces = 1 > 2;
+
+// đúng thì thực hiện
+if (isSucces) {
+  console.log("Điều kiện đúng");
+}
+// sai thì thực thi
+else {
+  console.log("điều kiện sai");
+}
