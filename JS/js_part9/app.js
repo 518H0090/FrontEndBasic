9e7f85d1887207ed71ef72ddf795ef9b49405e79
518H0090 <nhoxhieuro5@gmail.com>
2022-05-11 08:21:32 +0700

    Number

---
+++ JS/js_part9/app.js
@@ -0,0 +1,19 @@
+// cách 1
+var age = 18;
+var PI = 3.14;
+// cách 2
+var otherNumber = new Number(9);
+
+// nên chọn cách 1
+
+var result = 20 / "ABC";
+console.log(isNaN(result));
+
+var ageTo = 20;
+
+console.log(ageTo.toString());
+
+var numberVAlue = 20.3535;
+
+console.log(numberVAlue.toFixed(2));
+console.log(typeof numberVAlue.toFixed(2));
