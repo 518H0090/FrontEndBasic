bada9f8e8f6f6650f899b6f69058d59fb1db634b
518H0090 <nhoxhieuro5@gmail.com>
2022-05-11 20:40:21 +0700

    Date

---
+++ JS/js_part13/app.js
@@ -0,0 +1,12 @@
+// Date
+var date = new Date();
+
+console.log(typeof date);
+
+// Trả về string
+console.log(date.getFullYear());
+
+// getMonth đi từ 0 - 11
+console.log(date.getMonth() + 1);
+console.log(date.getDay());
+console.log(date.getDate());
