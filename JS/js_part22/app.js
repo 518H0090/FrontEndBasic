a766ed5b2d4cf1a99f52b9e2fe1bffab0992b5ed
518H0090 <nhoxhieuro5@gmail.com>
2022-05-18 20:30:05 +0700

    data type casting

---
+++ JS/js_part22/app.js
@@ -0,0 +1,24 @@
+// Type conversion
+// change the data type of value to another (string => boolean, number,float,byte)
+
+// string only
+var valueString = "20";
+valueString += 1;
+console.log(valueString);
+
+// convert string to number
+var age = Number(20);
+age += 1;
+console.log(age);
+
+// convert number to String
+var x = 20;
+valueX = String(x);
+console.log(valueX);
+
+// convert string to boolean
+let isTrueString = "True";
+console.log(isTrueString);
+let isTrue = Boolean(isTrueString);
+
+console.log(isTrue);
